cc1d37095094e4dde8f7434a896ecd08827e51de
heycalmdown <heyacct@gmail.com>
2015-09-06 21:50:03 +0900

    emacs) Add typescript mode

---
+++ .emacs.d/elpa/typescript-mode-20150830.1828/typescript-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "typescript-mode" "20150830.1828" "Major mode for editing typescript" 'nil :url "http://github.com/ananthakumaran/typescript.el" :keywords '("typescript" "languages"))
